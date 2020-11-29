e8c2e5998fefb3b9adbf2faf2fae4e0c45f79bc8
R. Richards <rdrrichards@hotmail.com>
2020-11-29 14:52:42 -0600

    ng update to latest

--- src/app/img-viewer/img-viewer.component.spec.ts
+++ src/app/img-viewer/img-viewer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ImgViewerComponent } from './img-viewer.component';
 
@@ -6,7 +6,7 @@ describe('ImgViewerComponent', () => {
   let component: ImgViewerComponent;
   let fixture: ComponentFixture<ImgViewerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ImgViewerComponent ]
     })
